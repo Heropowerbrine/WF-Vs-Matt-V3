7375e6a66d4496cc27f832a199fe4e55acf94d27
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2025-04-08 08:04:57 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -17,7 +17,9 @@ import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
 import lime.app.Application;
+import lime.system.System as LimeSystem;
 import states.TitleState;
+import mobile.states.CopyState;
 
 #if linux
 import lime.graphics.Image;
@@ -60,14 +62,22 @@ class Main extends Sprite
 
 	public function new()
 	{
+		#if mobile
+ 		#if android
+ 		SUtil.doPermissionsShit();
+ 		#end
+ 		Sys.setCwd(SUtil.getStorageDirectory());
+ 		#end
 		super();
 
 		// Credits to MAJigsaw77 (he's the og author for this code)
-		#if android
-		Sys.setCwd(Path.addTrailingSlash(Context.getExternalFilesDir()));
-		#elseif ios
-		Sys.setCwd(lime.system.System.applicationStorageDirectory);
-		#end
+		//#if android
+		//Sys.setCwd(Path.addTrailingSlash(Context.getExternalFilesDir()));
+		//#elseif ios
+		//Sys.setCwd(lime.system.System.applicationStorageDirectory);
+		//#end
+
+		mobile.backend.CrashHandler.init();
 
 		if (stage != null)
 		{
@@ -91,6 +101,7 @@ class Main extends Sprite
 
 	private function setupGame():Void
 	{
+		#if (openfl <= "9.2.0")
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
 
@@ -102,14 +113,18 @@ class Main extends Sprite
 			game.width = Math.ceil(stageWidth / game.zoom);
 			game.height = Math.ceil(stageHeight / game.zoom);
 		}
+		#else
+ 		if (game.zoom == -1.0)
+ 			game.zoom = 1.0;
+ 		#end
 	
 		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
 		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
-		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
-
-		#if !mobile
+		addChild(new FlxGame(game.width, game.height, #if (mobile && MODS_ALLOWED) CopyState.checkExistingFiles() ? game.initialState : CopyState #else game.initialState #end, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+		
+		
 		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -117,7 +132,7 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.data.showFPS;
 		}
-		#end
+		
 
 		FlxG.game.soundTray.volumeDownSound = "assets/shared/sounds/wiiTextHover";
 		FlxG.game.soundTray.volumeUpSound = "assets/shared/sounds/wiiTextHover";
@@ -140,8 +155,15 @@ class Main extends Sprite
 		DiscordClient.prepare();
 		#end
 
+		#if android FlxG.android.preventDefaultKeys = [BACK]; #end
+
+		LimeSystem.allowScreenTimeout = ClientPrefs.data.screensaver; 		
+ 		FlxG.scaleMode = new MobileScaleMode();
+
 		// shader coords fix
 		FlxG.signals.gameResized.add(function (w, h) {
+		     if(fpsVar != null)
+ 		           fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
 		     if (FlxG.cameras != null) {
 			   for (cam in FlxG.cameras.list) {
 				if (cam != null && cam.filters != null)
